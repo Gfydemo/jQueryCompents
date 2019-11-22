48dda14d9a8f833715061a6818f26f3db228e80f
耿飞阳 <1053221101@qq.com>
2019-11-22 18:13:07 +0800

    数据加载表格初步实现

--- dataTable/js/dataTable.js
+++ dataTable/js/dataTable.js
@@ -5,14 +5,68 @@
                         return new dataTable(obj)
                 }
         })
+        /**
+         * 
+         * @param {表格参数} obj columns 表格表头 data 表格数据
+         */
         function dataTable(obj) {
+                var tableDomObj // 表格对应的JQuery对象
+                var tableHeadObj // 表头对应的JQuery对象
+                var tableBodyObj // 表格主体对应的JQuery对象
                 // 创建表格整体
                 var create = function() {
                        var target = $('#'+obj.target)
-                       target.append('<div>jquery Table 插件</div>')
+
+                       var tableDomId = obj.target+'Table'
+                       var tableHeadId = obj.target+'TableHead'
+                       target.append('<table id="'+tableDomId+'"></table>')
+                // 创建表头
+                        tableDomObj = $('#'+tableDomId)
+                        tableDomObj.css({
+                                width: obj.width ? obj.width+'px' : '100%'
+                        })
+                        var tableHeadDom = '<thead id="'+tableHeadId+'"></thead>'
+                        tableDomObj.append(tableHeadDom)
+                        tableHeadObj = $('#'+tableHeadId)
+                        var theadTr = $('<tr></tr>')
+                        tableHeadObj.append(theadTr)
+                        obj.columns.forEach(col => {
+                                var theadTrTd = $('<td>'+col.title+'</td>')
+                                theadTrTd.css({
+                                        'text-align': col.align? col.align : ''
+                                })
+                                theadTr.append(theadTrTd)
+                        });
+                        var tableBodyId = obj.target+'TableBody'
+                        tableDomObj.append('<tbody id="'+tableBodyId+'"></tbody>')
+                        tableBodyObj = $('#'+tableBodyId)
+                        if (obj.data) {
+                                createTable(obj.data)
+                        } else {
+                                ajaxtable()
+                        }
                 }
                 // 根据数据加载表格数据
-                var createTable = function(data) {}
+                var createTable = function(data) {
+                        
+                        data.forEach((row, index) => {
+                                var tbodyTr = $('<tr></tr>')
+                                tableBodyObj.append(tbodyTr)
+                                obj.columns.forEach(col => {
+                                        var tbodyTrTd
+                                        if (col.template) {
+                                                tbodyTrTd=$('<td>'+col.template(row, row[col.key])+'</td>')
+                                        } else {
+                                                tbodyTrTd=$('<td>'+row[col.key]+'</td>')
+                                        }
+                                        tbodyTr.append(tbodyTrTd)
+                                        tbodyTrTd.css({
+                                                'text-align': col.align? col.align : ''   
+                                        })
+                                })
+                        })
+                       
+                }
                 // 异步加载表格数据
                 var ajaxtable = function () {}
 
--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-$('#table').dataTable()
+var tableData = [
+        {name: '小明', age: '12', sex: '1'},
+        {name: '小花', age: '10', sex: '0'},
+]
+$('#table').dataTable({
+        columns: [{title: '姓名', key: 'name', align: 'center' },
+                {title: '性别', key: 'sex', template: function(row, item) {
+                                return item == '1' ? '男' : '女'
+                        }
+                },
+                {title: '年龄', key: 'age'},
+                {title: '操作', key: '', template: function(row, item) {
+                                return '<button>删除</button>'
+                        }
+                }
+        ],
+        data: tableData
+})
